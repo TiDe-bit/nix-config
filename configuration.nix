1daa870ac502455a16be9262d6457828fa1e9dbf
Tim Dellmann <tdellmann@ente.cool>
2024-08-13 22:22:50 +0200

    fix config for latest

--- configuration.nix
+++ configuration.nix
@@ -63,7 +63,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
+  # sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -93,6 +93,7 @@
   };
 
   nixpkgs.config.allowUnfree = true;
+  programs.nix-ld.enable = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Open ports in the firewall.
