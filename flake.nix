8c7ba0ea382dac2aed0e3b21dce2b87598093900
Tim Dellmann <tdellmann@ente.cool>
2024-08-25 22:41:42 +0200

    prepare multiple machines

--- flake.nix
+++ flake.nix
@@ -10,28 +10,45 @@
     # };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./configuration.nix
-        ./modules/hyprland.nix
-        ./modules/shell.nix
-        ./modules/base.nix
-        ./modules/private.nix
-        # inputs.home-manager.nixosModules.default
-      ];
-    };
-
-    nixosConfigurations.gaming = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./configuration.nix
-        ./modules/hyprland.nix
-        ./modules/shell.nix
-        ./modules/base.nix
-        ./modules/gaming.nix
-      ];
-    };
+  outputs = { self, nixpkgs, ... }@inputs: 
+    let 
+      modules = import ./modules/all.nix { inherit (nixpkgs) lib; };
+    in
+    {
+      nixosConfigurations = {
+        tidy = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./machines/lil-thinkpad/configuration.nix
+            {
+              _module.args = {
+                inherit inputs;
+              };
+            }
+          ] ++ modules.nixos;
+        };
+      };
+    # nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+    #   specialArgs = {inherit inputs;};
+    #   modules = [
+    #     ./configuration.nix
+    #     ./modules/hyprland.nix
+    #     ./modules/shell.nix
+    #     ./modules/base.nix
+    #     ./modules/private.nix
+    #     # inputs.home-manager.nixosModules.default
+    #   ];
+    # };
+    # nixosConfigurations.gaming = nixpkgs.lib.nixosSystem {
+    #   specialArgs = {inherit inputs;};
+    #   modules = [
+    #     ./configuration.nix
+    #     ./modules/hyprland.nix
+    #     ./modules/shell.nix
+    #     ./modules/base.nix
+    #     ./modules/gaming.nix
+    #     # inputs.home-manager.nixosModules.default
+    #   ];
+    # };
   };
 }
