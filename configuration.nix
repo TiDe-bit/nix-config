ee179a862014e63fd8c8d00fff20dd0f27686579
Tim Dellmann <tdellmann@ente.cool>
2024-08-25 21:56:15 +0200

    on the misson to destroy the hyprcursor

--- configuration.nix
+++ configuration.nix
@@ -84,6 +84,7 @@
   # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
+
   users.users.tdellmann = {
     isNormalUser = true;
     description = "tdellmann";
